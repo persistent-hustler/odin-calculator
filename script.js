f079d4d677e5fbbcd0ba306e575921669f1ccda8
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-19 01:59:26 +0530

    Remove redundant break statement

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 function operate(operand1,operator,operand2) {
     switch(operator) {
         case 'plus': return +operand1 + +operand2;
-        break;
         case 'minus': return +operand1 - +operand2;
-        break;
         case 'times': return +operand1 * +operand2;
-        break;
         case 'divide-by': return +operand1/+operand2;
-        break;
         default: return 'ERROR';
     }
 }
