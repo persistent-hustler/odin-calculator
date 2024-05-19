f60939d3e1dbb120e08b807e08a46d8c1187ec84
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-19 20:18:44 +0530

    Connect the UI to the JavaScript

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ function operate(operand1,operator,operand2) {
         default: return 'ERROR';
     }
 }
+
+let calculator = document.querySelector('.calculator');
+let display = document.querySelector('.display');
+
+calculator.addEventListener('click', function(event) {
+    let id = event.target.id;
+
+    switch(id) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            break;
+        case 'plus':
+        case 'minus':
+        case 'times':
+        case 'divide-by':
+            break;
+        case 'equals':
+    }
+});
