679a8ca230ee9e890b7052136e26787d022fcc76
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-20 10:06:34 +0530

    Handle negative number inputs

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function operate(operand1,operator,operand2) {
-    switch(operator) {
+function operate(operand1, operator, operand2) {
+    switch (operator) {
         case 'plus': return +operand1 + +operand2;
         case 'minus': return +operand1 - +operand2;
         case 'times': return +operand1 * +operand2;
-        case 'divide-by': return +operand1/+operand2;
+        case 'divide-by': return +operand1 / +operand2;
         default: return 'ERROR';
     }
 }
@@ -15,37 +15,38 @@ let operator = null;
 let operand2 = null;
 let displayValue = '0';
 
-calculator.addEventListener('click', function(event) {
+calculator.addEventListener('click', function (event) {
     let id = event.target.id;
 
-    switch(id) {
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9': if(displayValue.length < 8) {
-                    displayValue = (displayValue==='0')? id: (displayValue + id);
-                    display.textContent = displayValue;
-                }
-        break;
-        case 'plus':
-        case 'minus':
-        case 'times':
-        case 'divide-by':
-        case 'equals': if(operand1 && operator) {
-            operand2 = display.textContent;
-            let result = operate(operand1,operator,operand2);
-            display.textContent = result;
-            operand2 = null;
-            operator = null;
-        }
-        displayValue = '0';
-        operand1 = display.textContent;
-        if(id!== 'equals') {operator = id;}
+    switch (id) {
+        case '.': if (displayValue.includes('.')) return;
+        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+            if (displayValue.length < 8) {
+                displayValue = (displayValue === '0') ? id : (displayValue + id);
+                display.textContent = displayValue;
+            }
+            break;
+        case 'plus': case 'minus': case 'times': case 'divide-by': case 'equals':
+            if(displayValue==='0' && id==='minus') {
+                displayValue = '-';
+                display.textContent = displayValue;
+                return;
+            }
+            if (operand1 && operator) {
+                operand2 = display.textContent;
+                let result = operate(operand1, operator, operand2);
+                display.textContent = result;
+                operand2 = null;
+                operator = null;
+            }
+            displayValue = '0';
+            operand1 = display.textContent;
+            if (id !== 'equals') { operator = id; }
+    }
+
+    if (parseInt(display.textContent) > 99999999 && parseInt(display.textContent) !== 'Infinity') {
+        display.textContent = 'too big';
+    } else if((parseInt(display.textContent)< -9999999 && parseInt(display.textContent)!== '-Infinity')) {
+        display.textContent='too small';
     }
 });
